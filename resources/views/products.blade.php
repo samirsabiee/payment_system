821ad3d524ce0f5ce8774c30b3ccf1fbeabfdf2a
samsab1995 <samirsabiee@gmail.com>
2021-08-26 15:03:53 +0430

    create products blade

---
+++ resources/views/products.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row d-flex flex-row justify-content-around">
+        @forelse($products as $product)
+            <div class="card col-2 p-0 m-1">
+                <img src="{{ $product->image }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $product->title }}</h5>
+                    <p class="card-text">{{ $product->description }}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        @empty
+            <h6 class="text-center">No Product Exist!</h6>
+        @endforelse
+    </div>
+@endsection
