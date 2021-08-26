e0a0ac32608212bc5131cd55bb1f056fd852b92d
samsab1995 <samirsabiee@gmail.com>
2021-08-26 17:23:53 +0430

    add hasStock func

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function hasStock($quantity): bool
+    {
+        return $this->stock >= $quantity;
+    }
 }
