c68d82e85cf69c5159800f26efdd6ccf42eb5180
samsab1995 <samirsabiee@gmail.com>
2021-08-26 15:54:53 +0430

    create SessionStorage contract and class

---
+++ app/Support/Storage/Contracts/StorageInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Support\Storage\Contracts;
+
+
+interface StorageInterface
+{
+    public function get($index);
+
+    public function set($index, $value);
+
+    public function all();
+
+    public function exist($index);
+
+    public function unset($index);
+
+    public function clear();
+}
---
+++ app/Support/Storage/SessionStorage.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace App\Support\Storage;
+
+
+use Countable;
+use Session;
+
+class SessionStorage implements Contracts\StorageInterface, Countable
+{
+    private $bucket;
+
+    /**
+     * SessionStorage constructor.
+     * @param string $bucket
+     */
+    public function __construct(string $bucket = 'default')
+    {
+        $this->bucket = $bucket;
+    }
+
+
+    public function get($index)
+    {
+        return Session::get($this->bucket . "." . $index);
+    }
+
+    public function set($index, $value)
+    {
+        Session::put($this->bucket . "." . $index, $value);
+    }
+
+    public function all()
+    {
+        return Session::get($this->bucket);
+    }
+
+    public function exist($index): bool
+    {
+        return Session::has($this->bucket . "." . $index);
+    }
+
+    public function unset($index)
+    {
+        Session::forget($this->bucket . "." . $index);
+    }
+
+    public function clear()
+    {
+        Session::forget($this->bucket);
+    }
+
+    public function count()
+    {
+        // TODO: Implement count() method.
+    }
+}
