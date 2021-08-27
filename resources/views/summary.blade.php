429e30d9922925d749790a61a1466aa67865102c
samsab1995 <samirsabiee@gmail.com>
2021-08-27 11:10:29 +0430

    separate payment block

---
+++ resources/views/summary.blade.php
@@ -0,0 +1,24 @@
+@inject('basket','App\Support\Basket\Basket')
+<div class="card">
+    <div class="card-header">
+        Payment
+    </div>
+    <div class="card-body">
+        <div class="row d-inline-flex-flex-row mt-4">
+            <h6 class="col-5 text-center">Total</h6>
+            <span class="col-5 font-weight-bold text-center p-0 m-0">{{ number_format($basket->subTotal()) }} $</span>
+        </div>
+        <hr>
+        <div class="row d-inline-flex-flex-row">
+            <h6 class="col-5 text-center">Transport Cost</h6>
+            <span class="col-5 font-weight-bold text-center p-0 m-0">10 $</span>
+        </div>
+        <hr>
+        <div class="row d-inline-flex-flex-row mb-4">
+            <h6 class="col-5 text-center">To Pay</h6>
+            <span
+                class="col-5 font-weight-bold text-center p-0 m-0">{{ number_format($basket->subTotal() + 10) }} $</span>
+        </div>
+    </div>
+</div>
+<a href="#" class="btn btn-primary btn-block mt-4">Continue To pay</a>
