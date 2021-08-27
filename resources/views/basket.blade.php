3594b3d9a11780df185fd82794e1c25ef6d98b35
samsab1995 <samirsabiee@gmail.com>
2021-08-27 12:58:51 +0430

    change position of payment button

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -14,18 +14,36 @@
                 <tbody>
                 @forelse($products as $product)
                     <tr>
-                        <td>{{ $product->title }}</td>
-                        <td>{{ $product->price . '$' }}</td>
-                        <td>{{ $product->quantity }}</td>
+                        <td class="col-4">{{ $product->title }}</td>
+                        <td class="col-4">{{ $product->price . '$' }}</td>
+                        <td class="col-4">
+                            <form method="post" action="{{ route('basket.update', $product->id) }}">
+                                @csrf
+                                <div class="input-group">
+                                    <select class="custom-select" id="quantity" name="quantity"
+                                            aria-label="Quantity Product">
+                                        @for($i = 1; $i <= $product->stock; $i++)
+                                            <option
+                                                {{ $i == $product->quantity ? 'selected' : '' }} value="{{ $i }}">{{ $i }}</option>
+                                        @endfor
+                                    </select>
+                                    <div class="input-group-append">
+                                        <button class="btn btn-outline-secondary" type="submit"> Update</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </td>
                     </tr>
                 @empty
-                    <p class="text-center font-weight-bolder m-3">Basket is empty click and go to <a href="{{ route('products.index') }}">products</a> page</p>
+                    <p class="text-center font-weight-bolder m-3">Basket is empty click and go to <a
+                            href="{{ route('products.index') }}">products</a> page</p>
                 @endforelse
                 </tbody>
             </table>
         </div>
         <div class="col-3">
             @include('summary')
+            <a href="{{ route('basket.checkout.form') }}" class="btn btn-primary btn-block mt-4">Continue To pay</a>
         </div>
     </div>
 @endsection
