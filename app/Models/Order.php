609cc9013e77c4a035b6b5d9071e95d85d2b6af3
samsab1995 <samirsabiee@gmail.com>
2021-08-27 15:51:50 +0430

    complete fillable array and productions relation

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'code', 'amount'];
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class)->withPivot('quantity');
+    }
 }
