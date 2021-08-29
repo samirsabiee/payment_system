9bfd00a78aa73783e08fdf34b43acf2a9b6d45e3
samsab1995 <samirsabiee@gmail.com>
2021-08-29 09:48:30 +0430

    edit summary blade

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -1,23 +1,20 @@
-@inject('basket','App\Support\Basket\Basket')
+@inject('cost','App\Support\Cost\Contracts\CostInterface')
 <div class="card">
     <div class="card-header">
         Payment
     </div>
     <div class="card-body">
-        <div class="row d-inline-flex-flex-row mt-4">
-            <h6 class="col-5 text-center">Total</h6>
-            <span class="col-5 font-weight-bold text-center p-0 m-0">{{ number_format($basket->subTotal()) }} $</span>
-        </div>
-        <hr>
-        <div class="row d-inline-flex-flex-row">
-            <h6 class="col-5 text-center">Transport Cost</h6>
-            <span class="col-5 font-weight-bold text-center p-0 m-0">10 $</span>
-        </div>
-        <hr>
+        @foreach($cost->getSummary() as $description => $price)
+            <div class="row d-inline-flex-flex-row mt-4">
+                <h6 class="col-5 text-center">{{ $description }}</h6>
+                <span class="col-5 font-weight-bold text-center p-0 m-0">{{ number_format($price) }} $</span>
+            </div>
+            <hr>
+        @endforeach
         <div class="row d-inline-flex-flex-row mb-4">
             <h6 class="col-5 text-center">To Pay</h6>
             <span
-                class="col-5 font-weight-bold text-center p-0 m-0">{{ number_format($basket->subTotal() + 10) }} $</span>
+                class="col-5 font-weight-bold text-center p-0 m-0">{{ number_format($cost->getTotalCost()) }} $</span>
         </div>
     </div>
 </div>
