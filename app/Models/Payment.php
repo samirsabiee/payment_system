4c890312ea5deb88e57f623c645fb685761e82ab
samsab1995 <samirsabiee@gmail.com>
2021-08-29 07:46:34 +0430

    add isOnline function payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -13,4 +13,9 @@ class Payment extends Model
     protected $attributes = [
         'status' => 0,
     ];
+
+    public function isOnline(): bool
+    {
+        return $this->method === 'online';
+    }
 }
