ef3144298bba1d5eda67588f25f3ca6aa2d327df
samsab1995 <samirsabiee@gmail.com>
2021-08-29 14:19:00 +0430

    add isExpired Method

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Coupon extends Model
 {
     use HasFactory;
+
+    public function isExpired(): bool
+    {
+        return Carbon::now()->isAfter(Carbon::parse($this->expire_time));
+    }
 }
