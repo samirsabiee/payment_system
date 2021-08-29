f72aa12a1f2c668eae5b9af806abfe8405a92d28
samsab1995 <samirsabiee@gmail.com>
2021-08-29 12:42:12 +0430

    add coupon block

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -16,5 +16,31 @@
             <span
                 class="col-5 font-weight-bold text-center p-0 m-0">{{ number_format($cost->getTotalCost()) }} $</span>
         </div>
+        @auth
+            @if(session()->has('coupon'))
+                <form method="get" action="{{ route('coupons.remove') }}">
+                    @csrf
+                    <hr>
+                    <div class="row input-group d-flex flex-row mt-2 justify-content-center align-items-center p-0">
+                        <h6 class="col=5 mx-2">Coupon</h6>
+                        <span class="mx-5">{{ session()->get('coupon')->code }}</span>
+                        <button type="submit" class="mx-2 btn btn-primary btn-sm">Remove</button>
+                    </div>
+                </form>
+            @else
+                <form method="post" action="{{ route('coupons.store') }}">
+                    @csrf
+                    <hr>
+                    <div class="row d-flex flex-row mt-2 justify-content-center align-items-center p-0">
+                        <h6 class="col=5">Coupon</h6>
+                        <div class="col-5 form-group p-0 m-0 mx-3">
+                            <input id="coupon" name="coupon" type="text" class="form-control" aria-label="Coupon"/>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-sm">Apply</button>
+                    </div>
+                </form>
+            @endif
+        @endauth
     </div>
 </div>
+@include('partials.validation-errors')
