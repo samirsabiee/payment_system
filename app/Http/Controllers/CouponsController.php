722066fbe5f80f909a30628c92027a99608a429b
samsab1995 <samirsabiee@gmail.com>
2021-08-29 14:19:36 +0430

    add auth middleware and validate coupon

--- app/Http/Controllers/CouponsController.php
+++ app/Http/Controllers/CouponsController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CouponRequest;
 use App\Models\Coupon;
+use App\Support\Discount\Coupon\CouponValidator;
+use App\Support\Discount\Coupon\Validator\Contracts\CouponValidatorInterface;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class CouponsController extends Controller
 {
+
+    private CouponValidator $validator;
+
+    /**
+     * CouponsController constructor.
+     * @param CouponValidator $validator
+     */
+    public function __construct(CouponValidator $validator)
+    {
+        $this->middleware('auth');
+        $this->validator = $validator;
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -21,6 +35,7 @@ class CouponsController extends Controller
     {
         try {
             $coupon = Coupon::where('code', $request->coupon)->firstOrFail();
+            $this->validator->isValid($coupon);
             session()->put(['coupon' => $coupon]);
             return redirect()->back()->with('success', true);
         } catch (\Exception $e) {
