17e077c4196b53c1dfb9369a785fb043da55fdba
samsab1995 <samirsabiee@gmail.com>
2021-08-30 23:04:49 +0430

    add validCoupons func

--- app/Support/Discount/Coupon/Traits/CouponAble.php
+++ app/Support/Discount/Coupon/Traits/CouponAble.php
@@ -5,11 +5,17 @@ namespace App\Support\Discount\Coupon\Traits;
 
 
 use App\Models\Coupon;
+use Illuminate\Support\Carbon;
 
 trait CouponAble
 {
     public function coupons()
     {
         return $this->morphMany(Coupon::class, 'couponable');
+    }
+
+    public function validCoupons()
+    {
+        return $this->coupons()->where('expire_time', '>', Carbon::now());
     }
 }
