8d2240b0ea440b22e26147d258bae3cfa6e07ee6
samsab1995 <samirsabiee@gmail.com>
2021-08-30 23:09:49 +0430

    add generateInvoice, payment, paid, downloadInvoice and invoicePath functions

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Facades\Storage;
+use niklasravnsborg\LaravelPdf\Facades\Pdf;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class Order extends Model
 {
@@ -21,5 +25,31 @@ class Order extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function generateInvoice()
+    {
+        $pdf = Pdf::loadView('invoice', ['order' => $this]);
+        return $pdf->save($this->invoicePath());
+    }
+
+    public function payment(): HasOne
+    {
+        return $this->hasOne(Payment::class);
+    }
+
+    public function paid(): bool
+    {
+        return $this->payment->status;
+    }
+
+    public function downloadInvoice(): StreamedResponse
+    {
+        return Storage::disk('public')->download('invoices/' . $this->id . '.pdf');
+    }
+
+    public function invoicePath(): string
+    {
+        return storage_path('app/public/invoices/') . $this->id . '.pdf';
     }
 }
